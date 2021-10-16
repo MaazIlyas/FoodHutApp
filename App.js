6e747cd770a54137faa4ee4714715e2a26bf900c
MaazIlyas <maazilyas@yahoo.com>
2021-10-16 17:03:26 +0930

    Update App.js
    Created the structure of the app

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import { NavigationContainer } from '@react-navigation/native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to ssstart working on your app!</Text>
     </View>
   );
 }
